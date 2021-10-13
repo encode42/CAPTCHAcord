674e12d82a9a72ee5d17c64b2c7ac869e24e7596
Encode42 <me@encode42.dev>
2021-10-12 23:23:12 -0400

    Let deno write the config files :facepalm:

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")" || exit
 if ! command -v vr &> /dev/null
 then
   echo "Velociraptor not found. Please install it at https://velociraptor.run/, as this script may become outdated."
-  deno run --allow-net --allow-read --allow-write=public/src/key.js --unstable --import-map=src/imports.json src/index.ts
+  deno run --allow-net --allow-read --allow-write=config/,public/src/key.js --unstable --import-map=src/imports.json src/index.ts
 else
   vr run watch
 fi
