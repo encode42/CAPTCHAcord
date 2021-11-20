91554b2f9b9fc0d7c0e1c814ab31d98623e4079c
Encode42 <me@encode42.dev>
2021-11-20 15:38:18 -0500

    Improve start script

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
 cd "$(dirname "$0")" || exit
 
-if ! command -v vr &> /dev/null
-then
-  echo "Velociraptor not found. Please install it at https://velociraptor.run/, as this script may become outdated."
-  deno run --allow-net --allow-read --allow-write=config/ --unstable --import-map=src/imports.json src/index.ts
+if ! command -v vr &> /dev/null; then
+    echo "Velociraptor not found. Please install it at https://velociraptor.run/, as this script may become outdated."
+    deno run --allow-net --allow-read --allow-write=config/ --unstable --import-map=src/imports.json src/index.ts
 else
-  vr run watch
+    ARG="start"
+    if [ "$1" == "watch" ]; then
+        ARG="watch"
+    fi
+
+    vr run $ARG
 fi
